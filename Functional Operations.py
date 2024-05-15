da7c2a3dd770da38c5a05c6ca2e0fdb531e524a7
Tandin Wangchuk <02230079.cst@rub.edu.bt>
2024-05-16 01:16:43 +0600

    Functional operation(Addition)

---
+++ Functional Operations.py
@@ -0,0 +1,8 @@
+while True:
+  a=int(input("Enter number:"))
+  b=int(input("Enter other number:"))
+  c=int(input("Enter number:"))
+  def add(a,b,c):
+    x=a+b+c
+    return x
+  print(add(a,b,c))
