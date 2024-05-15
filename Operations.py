a45acb0e74ce9e975075c8a0f31dc8887b55fcbc
Tandin Wangchuk <02230079.cst@rub.edu.bt>
2024-05-16 00:24:53 +0600

    Operations

---
+++ Operations.py
@@ -0,0 +1,11 @@
+name=input("Enter your name:\n")
+
+num1=float(input("Enter the number 1:\n"))
+num2=float(input("Enter the number 2:\n"))
+
+add_result = num1+num2
+substract_result = num1-num2
+multiply_result = num1*num2
+division_result = num1/num2
+print(f"Hello!{name} the sum of two number is: \n {add_result} \n  and the difference between the two number is:  \n {substract_result}.\n")
+d=input("my dog name is: ")
